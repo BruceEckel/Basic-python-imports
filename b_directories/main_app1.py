1203cf0d481f53f1cce4c0bce54e57958732b1f2
Bruce Eckel <BruceTEckel@gmail.com>
2023-04-18 09:51:51 -0600

    completeness

--- b_directories/main_app1.py
+++ b_directories/main_app1.py
@@ -1,5 +1,8 @@
 import lib1.f_lib
+import lib1.h_lib
 
 if __name__ == "__main__":
     lib1.f_lib.f()
+    lib1.f_lib.g()
+    lib1.h_lib.h()
 
--- b_directories/main_app2.py
+++ b_directories/main_app2.py
@@ -1,4 +1,7 @@
 from lib1 import f_lib
+from lib1 import h_lib
 
 if __name__ == "__main__":
     f_lib.f()
+    f_lib.g()
+    h_lib.h()
